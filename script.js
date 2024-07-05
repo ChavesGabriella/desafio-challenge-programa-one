8bcf2f9bea32c73d5cc79070451b25271c3be9f9
ChavesGabriella <gabriellasoeirooo@gmail.com>
2024-07-05 18:05:50 -0300

    criando a main, e feito o botão de criptografar, e inicio da estilização

---
+++ script.js
@@ -0,0 +1,12 @@
+function encryptText() {
+    let text = document.getElementById('inputText').value;
+    let encryptedText = text
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById('resultText').value = encryptedText;
+}
+
+
