200c382550ecd6eb82943627b2dcc04f0932ef68
ChavesGabriella <gabriellasoeirooo@gmail.com>
2024-07-08 17:12:34 -0300

    criação das 2 funções e feita a estilização restante da página

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ function encryptText() {
     document.getElementById('resultText').value = encryptedText;
 }
 
+function decryptText() {
+    let text = document.getElementById('resultText').value;
+    let decryptedText = text
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById('resultText').value = decryptedText;
+}
 
+function copyToClipboard() {
+    let resultText = document.getElementById('resultText');
+    resultText.select();
+    resultText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    alert("Texto copiado: " + resultText.value);
+}
